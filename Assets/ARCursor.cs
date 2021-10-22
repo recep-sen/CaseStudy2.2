9eacd7849729a88ebd793a5975c576e3c016d226
recep-sen <78692387+recep-sen@users.noreply.github.com>
2021-10-22 19:23:32 +0300

    added no cursor mode to script need to work on UI

--- Assets/ARCursor.cs
+++ Assets/ARCursor.cs
@@ -23,6 +23,19 @@ public class ARCursor : MonoBehaviour
         {
             UpdateCursor();
         }
+        if (useCursor)
+        {
+            GameObject.Instantiate(objectToPlace, transform.position, transform.rotation);
+        }
+        else if(Input.touchCount>0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            raycastManager.Raycast(Input.GetTouch(0).position, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+            if (hits.Count > 0)
+            {
+                GameObject.Instantiate(objectToPlace, hits[0].pose.position, hits[0].pose.rotation);
+            }
+        }
     }
     void UpdateCursor()
     {
