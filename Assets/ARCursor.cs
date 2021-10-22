6404c24ce059f5b70c0b1e99eeda84b8ca7dc3f4
recep-sen <78692387+recep-sen@users.noreply.github.com>
2021-10-22 19:05:07 +0300

    added cursor script needs work for spawning object

--- Assets/ARCursor.cs
+++ Assets/ARCursor.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class ARCursor : MonoBehaviour
 {
+    public GameObject cursorChildObject;
+    public GameObject objectToPlace;
+    public ARRaycastManager raycastManager;
+
+    public bool useCursor = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cursorChildObject.SetActive(useCursor);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (useCursor)
+        {
+            UpdateCursor();
+        }
+    }
+    void UpdateCursor()
+    {
+        Vector2 screenPosition = Camera.main.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(screenPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+        if(hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+        }
     }
 }
